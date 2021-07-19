3499c28fe860812b344c0c76bd3032ba2b6d0c58
Raja s <rajaaltus@gmail.com>
2021-07-19 07:21:55 +0700

    Part 2 done

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import { Link } from "gatsby";
+import React from "react";
+import Layout from "../components/Layout";
+
+const About = () => {
+  return (
+    <Layout pageTitle="About Page">
+      <p>Hi there! I'm the proud creator of this site, which i built with gatsby</p>
+    </Layout>
+  );
+};
+
+export default About;
